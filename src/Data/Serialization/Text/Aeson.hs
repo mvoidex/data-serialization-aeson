72763f75aa0e4c2c4b5fb5def9ea9f0d1fe3a3c5
Alexandr Ruchkin <voidex@live.com>
2012-10-21 16:45:30 +0400

    Forgot to add module

---
+++ src/Data/Serialization/Text/Aeson.hs
@@ -0,0 +1,24 @@
+-- | Module defines serializer for JSON to text
+--
+-- To serialize some value to text just chain 'json' and JSON-serializer for your date:
+--
+-- @
+-- my :: Jsonable MyData
+-- test = encode (json <~> my) (MyData 1 2 3)
+-- @
+--
+module Data.Serialization.Text.Aeson (
+    json
+    ) where
+
+import Data.ByteString (ByteString)
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Lazy as L
+import Data.Serialization
+import Data.Serialization.Text.Print
+import Data.Serialization.Text.Attoparsec
+
+import qualified Data.Aeson as A
+
+json :: SerializableT ByteString Print Atto A.Value
+json = serializable (printWith (B.concat . L.toChunks . A.encode)) (atto A.json)
